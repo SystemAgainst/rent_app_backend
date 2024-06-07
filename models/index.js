9100a45916dd46f5e170a998c828968ccd43acca
Tigran Dzhamalyan <tigr.SA2021@gmail.com>
2024-06-07 17:33:03 +0300

    add: created client

--- models/index.js
+++ models/index.js
@@ -1,13 +1,14 @@
+const sequelize = require('../config/db');
 const Lessor = require('./lessorModel');
 const Passport = require('./passportModel');
 const Apartment = require('./apartmentModel');
-const Client = require('./clientLessor');
+const Client = require('./clientModel');
 
 Lessor.belongsTo(Passport, { foreignKey: 'passport_id', as: 'passport' });
 Passport.hasOne(Lessor, { foreignKey: 'passport_id', as: 'lessor' });
 
 Apartment.belongsTo(Lessor, { foreignKey: 'lessor_id', as: 'lessor' });
-Lessor.hasMany(Apartment, { foreignKey: 'lessor_id', as: 'apartment' });
+Lessor.hasMany(Apartment, { foreignKey: 'lessor_id', as: 'apartments' });
 
 Client.belongsTo(Lessor, { foreignKey: 'lessorId', as: 'lessor' });
 Lessor.hasMany(Client, { foreignKey: 'lessorId', as: 'clients' });
